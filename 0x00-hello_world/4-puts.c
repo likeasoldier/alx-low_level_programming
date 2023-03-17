43daced4605e2a51095d4f2afb63d13f4b4dff25
likeasoldier <maximiliko@gmail.com>
2023-03-16 20:03:23 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * return: always o (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzl\n");
+		return(0);
+}
