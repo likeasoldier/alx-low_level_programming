588834baed8f670e1cfb073f690eb1bdcf6c4d98
likeasoldier <maximiliko@gmail.com>
2023-03-21 18:09:58 -0700

    nine

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,9 +14,9 @@ void times_table(void)
 	for (j = 0; j <= 9; j++)
 	{
 	if (j == 0)
-	printf("%2d", i * j);
+	printf("%d", i * j);
 	else
-	printf("%3d", i * j);
+	printf(", %d", i * j);
 	}
 	printf("\n");
 	}
