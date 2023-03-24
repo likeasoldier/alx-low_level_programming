95b454107d45950f0ec7c6c9639ad5ae39ae7218
likeasoldier <maximiliko@gmail.com>
2023-03-23 19:24:28 -0700

    102

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+/**
+ * print_number - prints an integer
+ * @n: integer to print
+ */
+void print_number(int n)
+{
+	int divisor = 1;
+
+	if (n < 0)
+	{
+	_putchar('-');
+	n = -n;
+	}
+
+	while (n / divisor >= 10)
+	{
+	divisor * = 10;
+	}
+
+	while (divisor > 0)
+	{
+	_putchar('0' + n / divisor);
+	n %= divisor;
+	divisor /= 10;
+	}
+}
