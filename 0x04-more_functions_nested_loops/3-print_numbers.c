bf6e8968d1d34f84243479a1414e9611d66b3f45
likeasoldier <maximiliko@gmail.com>
2023-03-23 17:24:07 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers 0 to 9, followed by a new line
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	_putchar(i + '0');  /* Convert digit to character and print */
+	}
+
+	_putchar('\n');  /* Print new line */
+}
