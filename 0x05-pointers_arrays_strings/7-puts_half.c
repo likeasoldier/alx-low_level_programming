ad221488960318a6f3e2a1dd29ef37c473536007
likeasoldier <maximiliko@gmail.com>
2023-03-27 14:05:15 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: pointer to string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len = 0, n;
+
+	while (*(str + len) != '\0')
+		len++;
+
+	if (len % 2 == 0)
+		n = len / 2;
+	else
+		n = (len - 1) / 2;
+
+	while (*(str + n) != '\0')
+	{
+		_putchar(*(str + n));
+		n++;
+	}
+
+	_putchar('\n');
+}
