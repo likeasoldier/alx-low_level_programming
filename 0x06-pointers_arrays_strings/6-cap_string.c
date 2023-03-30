ffb4bb52f5651fd8597fcdff892704bb27a8a5ea
likeasoldier <maximiliko@gmail.com>
2023-03-30 14:37:46 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,24 +1,30 @@
 #include "main.h"
 /**
- * cap_string - Capitalizes all words of a string.
+ * cap_string - capitalizes all words of a string
+ * @s: input string
  *
- * @s: String to be capitalized.
- *
- * Return: Pointer to the resulting string.
+ * Return: pointer to the resulting string
  */
-
 char *cap_string(char *s)
 {
 	int i;
+	char separators[] = " \t\n,;.!?\"(){}";
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= 'a' - 'A';
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (i == 0 || s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n'
-		    || s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' || s[i - 1] == '!'
-		    || s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '(' || s[i - 1] == ')'
-		    || s[i - 1] == '{' || s[i - 1] == '}')
-			s[i] = s[i] >= 'a' && s[i] <= 'z' ? s[i] - 32 : s[i];
-	}
+		int j;
 
+		for (j = 0; separators[j] != '\0'; j++)
+		{
+			if (s[i] == separators[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				s[i + 1] -= 'a' - 'A';
+				break;
+			}
+		}
+	}
 	return (s);
 }
