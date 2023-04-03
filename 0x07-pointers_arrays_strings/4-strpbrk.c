9f070a37e28963161ab0d644794b67108398ba62
likeasoldier <maximiliko@gmail.com>
2023-04-03 15:36:22 -0700

    4.1

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,18 +9,15 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
-
-	for (i = 0; s[i] != '\0'; i++)
+	int k;
+	while (*s)
 	{
-	for (j = 0; accept[j] != '\0'; j++)
-	{
-	if (s[i] == accept[j])
-	{
-		return (s + i);
+		for (k = 0; accept[k]; k++)
+		{
+		if (*s == accept[k])
+		return (s);
+		}
+	s++;
 	}
-	}
-	}
-
-	return (NULL);
+	return ('\0');
 }
