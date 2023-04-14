22773388fb6892839237a3c8e1aaa98d187567cf
likeasoldier <maximiliko@gmail.com>
2023-04-14 02:20:22 -0700

    3rd

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,4 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int main(int argc __attribute__((unused)), char *argv[]);
 int _putchar(char c);
 int main(int argc, char *argv[]);
 int main(int argc, char **argv);
+int _atoi(char *s);
+
+#endif
